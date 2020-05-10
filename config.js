bff54b02918d04bd4999ab62a6c7991b80f135fa
Alexander Kunup <a.kunup@shipnext.com>
2020-05-10 20:33:51 +0300

    Add hostname 99.80.183.117

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || '80';
 console.log({ PORT });
 
 module.exports = {
-  hostname: 'fixie:xZJtGPNC6EXuRIx@olympic.usefixie.com',
+  hostname: '99.80.183.117',
   ports: [PORT],
   apiUrl: 'io.cargo.lt',
   getTokenPath: '/accounts/signin',
