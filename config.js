3a756174fa3d47047f88d6cba4a623447878cbbe
Alexander Kunup <a.kunup@shipnext.com>
2020-05-10 16:47:31 +0300

    Change hostname to heroku 34.249.148.208

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 80;
 
 
 module.exports = {
-  hostname: 'cargo-extention-api.herokuapp.com',
+  hostname: '34.249.148.208',
   ports: [PORT],
   apiUrl: 'io.cargo.lt',
   getTokenPath: '/accounts/signin',
