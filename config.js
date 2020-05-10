d44d69a7bab44afd0ffdeada585ce41d77639500
Alexander Kunup <a.kunup@shipnext.com>
2020-05-10 17:13:50 +0300

    Add host

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const PORT = process.env.PORT || 443;
+const host = process.env.HOST || '0.0.0.0';
+
 console.log({ PORT });
+console.log({ host });
 
 module.exports = {
   hostname: 'cargo-extention-api.herokuapp.com',
